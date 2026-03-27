cc4c4c5930c5950a550ce77f468e94130b951af1
Farhan Nasim <f.nasim01@gmail.com>
2026-03-27 23:17:30 +0600

    initial commit.

---
+++ src/main/java/com/example/payzzle/PayzzleApplication.java
@@ -0,0 +1,13 @@
+package com.example.payzzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayzzleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayzzleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/payzzle/PayzzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payzzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayzzleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
