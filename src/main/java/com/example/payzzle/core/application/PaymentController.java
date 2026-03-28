9cefb4216f4aea02e8e2cd347027a0fab833684c
Farhan Nasim <f.nasim01@gmail.com>
2026-03-28 21:43:41 +0600

    implemented basic payment initiation and details collection flow.

---
+++ src/main/java/com/example/payzzle/core/application/PaymentController.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2026 Farhan Nasim
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.example.payzzle.core.application;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Farhan Nasim on 3/28/2026 1:24 AM
+ */
+@Controller
+public class PaymentController {
+
+    @GetMapping("/payment")
+    public String handlePayment(@RequestParam(name = "transaction_id") String transactionId,
+                                        @RequestParam(name = "amount") Integer amount,
+                                        @RequestParam(name = "currency") String currency,
+                                        @RequestParam(name = "success_url") String successUrl,
+                                        @RequestParam(name = "fail_url") String failUrl) {
+
+        return "/payment_details";
+    }
+}
